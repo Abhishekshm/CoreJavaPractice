10103fcb218f77dd311418bdbe57af18193908da
Abhishek sharma <100480882+Abhishekshm@users.noreply.github.com>
2022-09-07 14:48:22 +0530

    Add files via upload

---
+++ 5th sept/FirstDemo.java
@@ -0,0 +1,17 @@
+/*
+Program: This is the first java Program
+@author: Abhishek Sharma
+@date: 5th sept 2022
+
+*/
+
+//declaring class
+class FirstDemo
+{
+	//calling main method
+	public static void main(String args[])
+	{
+		System.out.println("Hello World"); //Printing hello world
+	}
+	//end of main
+}//end of class Firstdemo
---
+++ 5th sept/SimpleInterest.java
@@ -0,0 +1,24 @@
+/*
+Program: Simple Intrest
+@author: Abhishek Sharma
+@date: 6th sept 2022
+
+*/
+
+//declaring class
+class SimpleInterest
+{
+	//calling main method
+	public static void main(String args[])
+	{
+		float i;
+		float principal= 5000f;
+		float time= 2f;
+		float rate= 10f;
+		
+		i= (principal*rate*time)/100;
+		
+		System.out.println("The Simple Interest is: "+i);
+	}
+	//end of main
+}//end of class Firstdemo
